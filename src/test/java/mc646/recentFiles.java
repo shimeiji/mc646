904c8d867766e2b35916b559a08ad5b8fc269da5
shimeiji <hermes.shimizu@gmail.com>
2020-12-27 23:06:31 -0300

    Red 3.0

--- src/test/java/mc646/recentFiles.java
+++ src/test/java/mc646/recentFiles.java
@@ -10,24 +10,30 @@ import org.junit.Test;
 public class recentFiles {
 	
 	@Test
-	public void testRecentFilesLifo() {
+	public void testRecentFilesLimitSize3() {
 		
-		File file = new File("u6.pdf", "C:\\Users\\ra139511\\Desktop\\u6.pdf");
 		FileList recentList = new FileList();
-		
+		File file = new File("u6.pdf", "C:\\Users\\ra139511\\Desktop\\u6.pdf");
 		File file2 = new File("u6v2.pdf", "C:\\Users\\ra139511\\Desktop\\u6v2.pdf");
+		File file3 = new File("u6v3.pdf", "C:\\Users\\ra139511\\Desktop\\u6v3.pdf");
+		File file4 = new File("u6v4.pdf", "C:\\Users\\ra139511\\Desktop\\u6v4.pdf");
 		
 		recentList.addFile(file);
 		recentList.addFile(file2);
+		recentList.addFile(file3);
+		recentList.addFile(file4);
+		
 		
 		ArrayList<String> listNames = new ArrayList<String>();
-		listNames.add(0, "u6.pdf");
 		listNames.add(0, "u6v2.pdf");
+		listNames.add(0, "u6v3.pdf");
+		listNames.add(0, "u6v4.pdf");
 		assertEquals(listNames, recentList.getNameList());
 		
 		ArrayList<String> listPaths = new ArrayList<String>();
-		listPaths.add(0, "C:\\Users\\ra139511\\Desktop\\u6.pdf");
 		listPaths.add(0, "C:\\Users\\ra139511\\Desktop\\u6v2.pdf");
+		listPaths.add(0, "C:\\Users\\ra139511\\Desktop\\u6v3.pdf");
+		listPaths.add(0, "C:\\Users\\ra139511\\Desktop\\u6v4.pdf");
 		assertEquals(listPaths, recentList.getPathList());
 		
 		
