231ef815fb4d3bad23b113addda0b1c0d8c0fa3b
shimeiji <hermes.shimizu@gmail.com>
2020-12-27 22:45:54 -0300

    Green 2.0

--- src/test/java/mc646/FileList.java
+++ src/test/java/mc646/FileList.java
@@ -3,11 +3,26 @@ import java.util.*;
 
 public class FileList {
 	Collection<File> recentList = new ArrayList<File>();
+	
 	public void addFile(File file) {
 		this.recentList.add(file);
 	}
 	
 	public Collection<File> getRecentList(){
 		return this.recentList;
+	}
+	
+	public Collection<String> getNameList(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("u6.pdf");
+		list.add("u6v2.pdf");
+		return list;
+	}
+	
+	public Collection<String> getPathList(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("C:\\Users\\ra139511\\Desktop\\u6.pdf");
+		list.add("C:\\Users\\ra139511\\Desktop\\u6v2.pdf");
+		return list;
 	}
 }
