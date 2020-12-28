a2e65301dd5599f44c02318f1b222f1adb501226
shimeiji <hermes.shimizu@gmail.com>
2020-12-27 21:26:16 -0300

    Red 1

---
+++ src/test/java/mc646/recentFiles.java
@@ -0,0 +1,18 @@
+package mc646;
+
+import static org.junit.Assert.*;
+
+
+public class recentFiles {
+	
+	@Test
+	public void showRecentFiles() {
+		File file = new File();
+		FileList recentList = new FileList();
+		
+		recentList.addfile(file);
+		assertNull(recenList);
+		
+	}
+
+}
