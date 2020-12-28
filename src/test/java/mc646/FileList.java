34d9e0465eea6d2b0b7eb3f40fb525d63b163e11
shimeiji <hermes.shimizu@gmail.com>
2020-12-27 23:12:13 -0300

    Refactor 3.0

--- src/test/java/mc646/FileList.java
+++ src/test/java/mc646/FileList.java
@@ -5,6 +5,9 @@ public class FileList {
 	ArrayList<File> recentList = new ArrayList<File>();
 	
 	public void addFile(File file) {
+		if(recentList.size() > 2) {
+			recentList.remove(recentList.size()-1);
+		}
 		this.recentList.add(0, file);
 	}
 	
@@ -14,17 +17,17 @@ public class FileList {
 	
 	public Collection<String> getNameList(){
 		ArrayList<String> list = new ArrayList<String>();
-		list.add(0, "u6v2.pdf");
-		list.add(0, "u6v3.pdf");
-		list.add(0, "u6v4.pdf");
+		for (File e : recentList) {
+			list.add(e.getName());
+		}
 		return list;
 	}
 	
 	public Collection<String> getPathList(){
 		ArrayList<String> list = new ArrayList<String>();
-		list.add(0, "C:\\Users\\ra139511\\Desktop\\u6v2.pdf");
-		list.add(0, "C:\\Users\\ra139511\\Desktop\\u6v3.pdf");
-		list.add(0, "C:\\Users\\ra139511\\Desktop\\u6v4.pdf");
+		for (File e : recentList) {
+			list.add(e.getPath());
+		}
 		return list;
 	}
 }
